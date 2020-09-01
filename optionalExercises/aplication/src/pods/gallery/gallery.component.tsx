f87211e7a85b97120db6f6c71c4564f7e33caa81
Mariangel <maria.angel.delacueva@gmail.com>
2020-09-01 21:13:43 +0200

    adding pictures

--- optionalExercises/aplication/src/pods/gallery/gallery.component.tsx
+++ optionalExercises/aplication/src/pods/gallery/gallery.component.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { PictureInfo } from './gallery.vm';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
+
 
 interface Props {
   pictures: PictureInfo[];
@@ -15,17 +9,12 @@ interface Props {
 export const GalleryComponent: React.FC<Props> = ({ pictures }) => {
   return (
     <>
-    <div className={classes.root}>
       {pictures.map(e => (
         <li key={e.id}>
           {e.title}
-            <img
-              src={`src/assets/${e.picUrl}`}
-              style={{ width: '10rem' }}
-            ></img>
+          <img src={`src/assets/${e.picUrl}`} style={{ width: '10rem' }}></img>
         </li>
       ))}
-      </div>
     </>
   );
 };
